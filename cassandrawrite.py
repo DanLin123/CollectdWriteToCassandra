205f9bae57eca4e66b22debe93f656ba124fbab7
lindanXmu <lindan_xmu@126.com>
2013-02-26 21:16:57 +0800

    First commit

--- cassandrawrite.py
+++ cassandrawrite.py
@@ -6,24 +6,27 @@ from pycassa.pool import ConnectionPool
 from pycassa.columnfamily import ColumnFamily
 import time
 
-
 def write(vl, data=None):
 #    get connection from pycassa connection pool 
 #    创建keyspace Rawdata保存监控数据
     pool = ConnectionPool('Monitor', ['localhost:9160'])
-#   创建columnFamily：Time
-    col_fam = pycassa.ColumnFamily(pool, 'RawData')
-#   加入时间进行分区
-    timeString = time.strftime("%Y-%m-%d", time.localtime(vl.time))
+#   创建columnFamily：RawData, Rollups60, Rollups300, Rollups7200, Rollups86400
+    col_fam_rawdata = pycassa.ColumnFamily(pool, 'rawdata')    
+    
+#   加入时间进行分区,以月作为分区
+    timeString = time.strftime("%Y-%m", time.localtime(vl.time))
     key = [str(vl.host), str(vl.plugin), str(vl.plugin_instance), str(vl.type), str(vl.type_instance), timeString]
     keyString = "#".join(key)
-
+    
+    
     for i in vl.values:
-        col_fam.insert(keyString, {vl.time: i})
+        col_fam_rawdata.insert(keyString, {vl.time: i})     #插入RawData
         
+
 #   同时写入一个文件作为测试
     with open('/tmp/workfile', 'a') as f:
-		f.write(keyString + " " + str(vl.time) + " " + str(i) + "\n")
+        f.write(keyString + " " + str(vl.time) + " " + str(i) + "\n")
+     
     pool.dispose()
     f.close()
 try:
